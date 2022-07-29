f665d9c80ad680de7309240634ecef5ee0195869
wave103 <wave103x@gmail.com>
2022-07-29 15:37:11 +0300

    feat: add weather localstorage functionality

--- momentum/script.js
+++ momentum/script.js
@@ -1,36 +1,56 @@
 /*      weather          */
+const weatherWordInput = document.querySelector('.input-weather-city');
+let weatherSearchWord = '';
 
-fetch("https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/Minsk?unitGroup=metric&key=4BSGFC52XJ96N6F5WJFYM8M4P&contentType=json", {
-    "method": "GET",
-    "headers": {
+weatherWordInput.addEventListener('keyup', e => {
+    if (e.key === 'Enter' || e.keyCode === 13) {
+        if (weatherSearchWord === '' || weatherSearchWord === null) {
+            document.querySelector('.temperature').textContent = 'Invalid city';
+            document.querySelector('.wind-speed').textContent = '';
+            document.querySelector('.humidity').textContent = '';
+        } else {
+            localStorage.setItem('weatherCity', JSON.stringify(weatherSearchWord));
+            weatherUpdate(weatherSearchWord);
+        }
     }
+
 })
-    .then(function (response) { return response.json() })
-    .then(response => {
-        console.log(response);
-        document.querySelector('.city-name').textContent = response.resolvedAddress;
-        document.querySelector('.temperature').innerHTML = response.currentConditions.feelslike + '&deg; ' + response.currentConditions.conditions;
-        document.querySelector('.wind-speed').textContent = 'Wind speed: ' + response.currentConditions.windspeed + 'm/s';
-        document.querySelector('.humidity').textContent = 'Humidity: ' + response.currentConditions.humidity + '%';
-        // document.querySelector('.weather-icon').innerHTML = `<img src="https://raw.githubusercontent.com/visualcrossing/WeatherIcons/73c8cc581d8d35076b47047088f3bc91cb1dd675/SVG/1st%20Set%20-%20Monochrome/${response.currentConditions.icon}.svg">`;
+
+function weatherUpdate(city='Minsk') {
+    fetch(`https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/${city}?unitGroup=metric&key=4BSGFC52XJ96N6F5WJFYM8M4P&contentType=json`, {
+        "method": "GET",
+        "headers": {
+        }
     })
-    .catch(err => {
-        console.error(err);
-    });
+        .then(function (response) { return response.json() })
+        .then(response => {
+            // document.querySelector('.city-name').textContent = response.resolvedAddress;
+            document.querySelector('.temperature').innerHTML = response.currentConditions.feelslike + '&deg; ' + response.currentConditions.conditions;
+            document.querySelector('.wind-speed').textContent = 'Wind speed: ' + response.currentConditions.windspeed + 'm/s';
+            document.querySelector('.humidity').textContent = 'Humidity: ' + response.currentConditions.humidity + '%';
+            // document.querySelector('.weather-icon').innerHTML = `<img src="https://raw.githubusercontent.com/visualcrossing/WeatherIcons/73c8cc581d8d35076b47047088f3bc91cb1dd675/SVG/1st%20Set%20-%20Monochrome/${response.currentConditions.icon}.svg">`;
+        })
+        .catch(err => {
+            console.error(err);
+        });
+}
+if (localStorage.getItem('weatherCity')) {
+    weatherWordInput.value = JSON.parse(localStorage.getItem('weatherCity'));
+    weatherUpdate(JSON.parse(localStorage.getItem('weatherCity')));
+} else weatherUpdate();
 
 
 /*      backgorund       */
-
-
+let bgSearchWord = '';
 let hours = new Date().getHours();
-if (+hours > 5 && +hours < 12) bgSearchWord = 'morning';
-if (+hours > 12 && +hours < 18) bgSearchWord = 'afternoon';
-if (+hours > 18 && +hours < 24) bgSearchWord = 'evening';
-if (+hours > 0 && +hours < 6) bgSearchWord = 'night';
-
+if (+hours >= 6 && +hours < 12) bgSearchWord = 'morning';
+if (+hours >= 12 && +hours < 18) bgSearchWord = 'afternoon';
+if (+hours >= 18 && +hours < 24) bgSearchWord = 'evening';
+if (+hours >= 0 && +hours < 6) bgSearchWord = 'night';
 fetch(`https://api.unsplash.com/search/photos?query=${bgSearchWord}&client_id=uGY17bGixkwPCRtDhfelM4l79HtvEwHTnAaBOz6KMko`, {
     "method": "GET",
-    "headers": {}
+    "headers": {},
+
 })
     .then(response => response.json())
     .then(response => {
@@ -38,8 +58,6 @@ fetch(`https://api.unsplash.com/search/photos?query=${bgSearchWord}&client_id=uG
 
         let imgIndex = 0;
         document.querySelector('.main').style.backgroundImage = `url(${response.results[imgIndex].urls.regular})`;
-
-
 
         document.querySelector('.arrow-left').addEventListener('click', () => {
             imgIndex > 8 ? imgIndex = 0 : imgIndex++;
@@ -49,16 +67,24 @@ fetch(`https://api.unsplash.com/search/photos?query=${bgSearchWord}&client_id=uG
             imgIndex < 1 ? imgIndex = 9 : imgIndex--;
             document.querySelector('.main').style.backgroundImage = `url(${response.results[imgIndex].urls.regular})`;
         })
+        for (let i = 0; i < response.results.length; i++) {
+            img = new Image();
+            img.onload = function () { };
+            img.src = response.results[i].urls.regular;
+        }
     })
+
     .catch(err => {
         console.error(err);
     });
 
 
+
 /*     center view      */
 
+//show time
 const todayTime = document.querySelector('.time');
-const sayHello = document.querySelector('.hello');
+const sayHello = document.querySelector('.hello-to-name');
 
 const dateUpdate = () => {
     let now = new Date();
@@ -68,19 +94,25 @@ const dateUpdate = () => {
     todayTime.textContent = `${hours} : ${minutes} : ${seconds}`;
     document.querySelector('.center-date').textContent = now.toLocaleDateString('en-UK', { weekday: 'long', month: 'long', day: 'numeric' });
 
-    if (+hours > 5 && +hours < 12) sayHello.textContent = 'Good morning,';
-    if (+hours > 12 && +hours < 18) sayHello.textContent = 'Good afternoon,';
-    if (+hours > 18 && +hours < 24) sayHello.textContent = 'Good evening,';
-    if (+hours > 0 && +hours < 6) sayHello.textContent = 'Good night,';
+    if (+hours >= 6 && +hours < 12) sayHello.textContent = 'Good morning,';
+    if (+hours >= 12 && +hours < 18) sayHello.textContent = 'Good afternoon,';
+    if (+hours >= 18 && +hours < 24) sayHello.textContent = 'Good evening,';
+    if (+hours >= 0 && +hours < 6) sayHello.textContent = 'Good night,';
 }
 dateUpdate();
 // setInterval(dateUpdate, 1000);
 
-document.querySelector('.inputName').size = 8;
+//get unputed name and update input width
+const inputName = document.querySelector('.inputName');
+inputName.size = 8;
+if (localStorage.getItem('myName')) {
+    inputName.value = localStorage.getItem('myName');
+    inputName.size = inputName.value.length;
+}
+
 
 
 /*      quoters     */
-
 
 fetch(`https://favqs.com/api/qotd`, {
     "method": "GET",
